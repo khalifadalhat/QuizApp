fbba3e9cfc62e3e25982db58da846641063ecf2b
khalifa <khalifadalhat@gmail.com>
2025-07-03 21:41:02 +0100

    Refactor App component to use Bootstrap for layout, replacing div with Container for improved structure and styling.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/Home";
 import Quiz from "./components/Quiz";
 import EndQuiz from "./components/EndQuiz";
 import { QuizContext } from "./Helpers/context";
+import { Container } from "react-bootstrap";
 
 function App() {
   const [initial, setInitial] = useState("home");
   const [score, setScore] = useState(0);
+
   return (
-    <div className="App">
-      <h1>Quiz app</h1>
+    <Container fluid className="app-container">
       <QuizContext.Provider value={{ initial, setInitial, score, setScore }}>
         {initial === "home" && <Home />}
         {initial === "quiz" && <Quiz />}
         {initial === "endquiz" && <EndQuiz />}
       </QuizContext.Provider>
-    </div>
+    </Container>
   );
 }
 
